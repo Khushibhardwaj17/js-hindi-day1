fdbcff121ecd7f25c0132f1336af4f7bb171c635
khushi <115889730+Khushibhardwaj17@users.noreply.github.com>
2024-01-17 12:19:15 +0000

    allinonejs

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,47 @@
-console.log("Khushi")
+/*console.log("Khushi")
+//use const for constant and let for declearing vaiables(we generally dont use var for variabe because gives a problem of block scope and functional scope)
+//const accountId = 1237
+let accountEmail = "dfjhg@google.com"
+accountPassword = "Jaipur"
+let accountState;
+accountId= 234
+accountEmail="dhkdhf@google.com"
+accountPassword="32764"
+accountCity="Bangaluru"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountState])
+*/
+
+
+
+
+
+// DATA TYPE
+
+
+
+
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+
+console.log(3+3); console.log("Khushi")//code readability is important
+//documantation-MDN and TC39
+
+//premitive datatype
+
+//number => 2^53
+//bigint
+//string=>""
+//boolean=>T/F
+//null(stand alone value)//value assigned is zero
+//undefined =>value not assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof "khushi")
+console.log(typeof null) //null is a object
+console.log(typeof undefined)//undefined is a data type
