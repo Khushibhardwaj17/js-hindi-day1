550b4fcf264ea99337247ae47cd23804f36e3f68
khushi <115889730+Khushibhardwaj17@users.noreply.github.com>
2024-01-19 22:59:28 +0000

    new

---
+++ 01_basics/date.js
@@ -0,0 +1,9 @@
+//date is a object in javascript
+/*let myDate = new Date()
+console.log(myDate.toString());
+let myCreatedDate = new Date(2023,0,23,5,3)
+console.log(myCreatedDate.toLocaleString())*/
+let myCreatedDate = new Date(2023,0,23,5,3)
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
