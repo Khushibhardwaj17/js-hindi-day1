5015005e71cdf75aacf41599d5b420e416b47ae6
khushi <115889730+Khushibhardwaj17@users.noreply.github.com>
2024-01-27 21:44:38 +0000

    arr in js

---
+++ 01_basics/arr2.js
@@ -0,0 +1,16 @@
+var emp=["Sonoo","Vimal","Ratan"];  
+for (i=0;i<emp.length;i++){  
+console.log(emp[i]);  
+}  
+
+//directly
+
+var i;  
+var emp = new Array();  
+emp[0] = "Arun";  
+emp[1] = "Varun";  
+emp[2] = "John";  
+  
+for (i=0;i<emp.length;i++){  
+console.log(emp[i]);  
+}  
